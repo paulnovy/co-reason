2b7c18ddb526ceaa59c5e92e9b49fa39871e5139
paulnovy <pl.lukasik@gmail.com>
2025-07-03 00:46:20 +0200

    Update database.py

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,10 +1,13 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
 
-SQLALCHEMY_DATABASE_URL = "postgres://postgres:superpass@localhost:5432/postgres"
+# 1. użyj prawidłowego dialektu
+SQLALCHEMY_DATABASE_URL = (
+    "postgresql+psycopg2://postgres:superpass@localhost:5432/postgres"
+)
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+# 2. dodaj future=True / echo=True w dev (bardziej czytelne logi)
+engine = create_engine(SQLALCHEMY_DATABASE_URL, echo=True, future=True)
 
+SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 Base = declarative_base()
