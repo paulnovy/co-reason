8a4d570ebfcf6ce450edb7c0ce9873e149a94025
paulnovy <pl.lukasik@gmail.com>
2025-07-03 01:32:15 +0200

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -9,3 +9,10 @@ app.include_router(variable_router)
 @app.get("/")
 def read_root():
     return {"msg": "ok"}
+from fastapi.middleware.cors import CORSMiddleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
